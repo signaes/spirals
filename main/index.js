25e3aa50568cb87c094b2f6c7c546e049e168348
Thiago Oliveira <thiago@signaes.co>
2016-12-21 06:49:31 -0200

    initial commit

---
+++ main/index.js
@@ -0,0 +1,36 @@
+const Box = value => ({
+  map: f => Box(f(value)),
+  expose: f => f(value),
+  inspect: () => `Box(${value})`
+});
+
+const Right = value => ({
+  map: f => Right(f(value)),
+  expose: f => f(value),
+  inspect: () => `Right(${value})`
+});
+
+const Left = value => ({
+  map: () => Left(value),
+  expose: () => value,
+  inspect: () => `Left(${value})`
+});
+
+const Maybe = value => ({
+  isNothing: () => value === null || value === undefined,
+  map: f => this.isNothing() ? Maybe(null) : Maybe(f(this.value)),
+  getValue: () => value,
+  inspect: () => `Maybe(${value})`
+});
+
+const Either = (left, right, value) => Maybe(value).isNothing()
+  ? left(value)
+  : right(value);
+
+module.exports = {
+  Box: Box,
+  Right: Right,
+  Left: Left,
+  Maybe: Maybe,
+  Either: Either
+};
