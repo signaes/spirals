babc5221a77895c37ad5a52e3eb9b9eff3129a0d
Thiago Oliveira <thiagooliveira@Macintosh.home>
2017-05-14 16:29:42 -0300

    feat: removes methods that will not be used for now

--- main/index.js
+++ main/index.js
@@ -1,21 +1,3 @@
-const Box = value => ({
-  map: f => Box(f(value)),
-  expose: f => f(value),
-  inspect: () => `Box(${value})`
-});
-
-const Right = value => ({
-  map: f => Right(f(value)),
-  expose: f => f(value),
-  inspect: () => `Right(${value})`
-});
-
-const Left = value => ({
-  map: () => Left(value),
-  expose: () => value,
-  inspect: () => `Left(${value})`
-});
-
 const Maybe = value => ({
   isNothing: () => value === null || value === undefined,
   map: f => Maybe(value).isNothing() ? Maybe(null) : Maybe(f(value)),
@@ -23,14 +5,11 @@ const Maybe = value => ({
   value
 });
 
-const Either = (left, right, value) => Maybe(value).isNothing()
+const Either = ({ left, right, value }) => Maybe(value).isNothing()
   ? left(value)
   : right(value);
 
 module.exports = {
-  Box: Box,
-  Right: Right,
-  Left: Left,
   Maybe: Maybe,
   Either: Either
 };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-export const Box = value => ({
-  map: f => Box(f(value)),
-  expose: f => f(value),
-  inspect: () => `Box(${value})`
-});
-
-export const Right = value => ({
-  map: f => Right(f(value)),
-  expose: f => f(value),
-  inspect: () => `Right(${value})`
-});
-
-export const Left = value => ({
-  map: () => Left(value),
-  expose: () => value,
-  inspect: () => `Left(${value})`
-});
-
 export const Maybe = value => ({
   isNothing: () => value === null || value === undefined,
   map: f => Maybe(value).isNothing() ? Maybe(null) : Maybe(f(value)),
@@ -23,6 +5,6 @@ export const Maybe = value => ({
   value
 });
 
-export const Either = (left, right, value) => Maybe(value).isNothing()
+export const Either = ({ left, right, value }) => Maybe(value).isNothing()
   ? left(value)
   : right(value);
