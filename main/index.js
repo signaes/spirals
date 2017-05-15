7a56105e2fe170bdcc9da34aae762baba4725e93
Thiago Oliveira <thiagooliveira@Macintosh.home>
2017-05-14 21:14:44 -0300

    feat: uses ES6 in src and generates main with rollup

--- main/index.js
+++ main/index.js
@@ -1,15 +1,28 @@
-const Maybe = value => ({
-  isNothing: () => value === null || value === undefined,
-  map: f => Maybe(value).isNothing() ? Maybe(null) : Maybe(f(value)),
-  inspect: () => `Maybe(${value})`,
-  value
-});
+'use strict';
 
-const Either = ({ left, right, value }) => Maybe(value).isNothing()
-  ? left(value)
-  : right(value);
+Object.defineProperty(exports, '__esModule', { value: true });
 
-module.exports = {
-  Maybe: Maybe,
-  Either: Either
+var Maybe = function Maybe(value) {
+  return {
+    isNothing: function isNothing() {
+      return value === null || value === undefined;
+    },
+    map: function map(f) {
+      return Maybe(value).isNothing() ? Maybe(null) : Maybe(f(value));
+    },
+    inspect: function inspect() {
+      return "Maybe(" + value + ")";
+    },
+    value: value
+  };
 };
+
+var Either = function Either(_ref) {
+  var left = _ref.left,
+      right = _ref.right,
+      value = _ref.value;
+  return Maybe(value).isNothing() ? left(value) : right(value);
+};
+
+exports.Maybe = Maybe;
+exports.Either = Either;
